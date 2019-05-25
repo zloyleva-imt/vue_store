3fe461f35ece85502cc2471ac4a12189e6829931
ZloyLeva <zloyleva@gmail.com>
2019-05-25 12:39:24 +0300

    add Header + CartHeader

---
+++ src/components/CartHeader.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="text-light">
+		Cart <span class="badge badge-secondary">0</span>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CartHeader'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">VueStore</b-navbar-brand>
+
+      <CartHeader/>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import CartHeader from "./CartHeader";
+export default {
+  name: "Header",
+  components: {
+    CartHeader
+  }
+};
+</script>
+
+<style>
+</style>
